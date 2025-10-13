91fed40c48b0a301b8097deb153a969144694baa
Aaron <aaronyee2005@gmail.com>
2025-10-12 23:25:21 -0700

    Got cmake working

---
+++ build/app/CMakeFiles/hello_world.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_world.dir/src/badmath.c.o"
+  "CMakeFiles/hello_world.dir/src/badmath.c.o.d"
+  "CMakeFiles/hello_world.dir/src/main.c.o"
+  "CMakeFiles/hello_world.dir/src/main.c.o.d"
+  "CMakeFiles/hello_world.dir/src/reaction_timer.c.o"
+  "CMakeFiles/hello_world.dir/src/reaction_timer.c.o.d"
+  "hello_world"
+  "hello_world.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hello_world.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/hal/CMakeFiles/hal.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/aaron/ensc351/work/assignment1/assignment1ENSC351/cmake_starter/hal/src/button.c" "hal/CMakeFiles/hal.dir/src/button.c.o" "gcc" "hal/CMakeFiles/hal.dir/src/button.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
