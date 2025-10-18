728c6c05ae6f1c24de66e2985b7f850e106dc5ec
Aaron <aaronyee2005@gmail.com>
2025-10-17 20:36:19 -0700

    FinalFinalCopy

--- build/app/CMakeFiles/reaction_timer.dir/src/badmath.c.o.d
+++
@@ -1,4 +0,0 @@
-app/CMakeFiles/reaction_timer.dir/src/badmath.c.o: \
- /home/aaron/ensc351/work/assignment1/assignment1ENSC351/cmake_starter/app/src/badmath.c \
- /usr/aarch64-linux-gnu/include/stdc-predef.h \
- /home/aaron/ensc351/work/assignment1/assignment1ENSC351/cmake_starter/app/include/badmath.h
--- build/app/CMakeFiles/reaction_timer.dir/src/reaction_timer.c.o.d
+++
@@ -1,3 +0,0 @@
-app/CMakeFiles/reaction_timer.dir/src/reaction_timer.c.o: \
- /home/aaron/ensc351/work/assignment1/assignment1ENSC351/cmake_starter/app/src/reaction_timer.c \
- /usr/aarch64-linux-gnu/include/stdc-predef.h
